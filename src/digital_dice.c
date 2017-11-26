5108e618063b8abfca4dd21eeb8c1f72ba465159
Lukas Jäger <lukas_jaeger@gmx.net>
2017-11-26 10:57:48 +0100

    Digital dice now functional

--- src/digital_dice.c
+++ src/digital_dice.c
@@ -3,28 +3,30 @@
 
 
 int main(){
+	// Watchdog and oscillator setup
 	WDTCTL = WDTPW + WDTHOLD;         
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 
+	// LED field setup
 	P2DIR |= BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5;
 	P2OUT = 0;
+	P1DIR |= BIT6;
 
-	P1DIR |= BIT7 | BIT6;
+	// Button setup
+ 	P1DIR &= ~BIT3;
+   	P1OUT |= BIT3;
+   	P1REN |= BIT3;
 
 	uint8_t num = 0;
 	while(1){
+		// Button was pressed
+		if ((P1IN & BIT3) == 0){ 
+			num++;
+			if (num > 6){
+				num = 1;
+			}
+		}
 		write_num(num);
-		num++;
-		if (num > 9) num = 0;
-		if (num % 2){
-			set_DP();
-		}
-		else{
-			clear_DP();
-		}
-		for (int i = 0 ; i < 1000; i++){
-			__delay_cycles(1000);	
-		}
 	}
 }
